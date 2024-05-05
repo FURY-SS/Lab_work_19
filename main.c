907c060fc18cac32fc0dbf574ab13cda1e2da42d
Vadim Ushakov <vadimushakov33@gmail.com>
2024-05-05 11:47:09 +0300

    adding tests for 3d ex.

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "tasks/matrix_transposition.h"
 #include "tasks/convert_float_num.h"
+#include "tasks/evaluate_expression.h"
+
 
 int main() {
     // 1
@@ -9,5 +11,8 @@ int main() {
     generate_float_file("C:\\Users\\Vadim\\CLionProjects\\lab_work_19\\tasks_tests\\task_2_test_0.txt", 10);
     test_convert_float();
     // 3
+    generate_expression("C:\\Users\\Vadim\\CLionProjects\\lab_work_19\\tasks_tests\\task_3_test_0.txt");
+    test_evaluate();
+    // 4
 
 }
