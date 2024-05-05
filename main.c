fa9331f995e1d471fe0043d601449e158a9263d7
Vadim Ushakov <vadimushakov33@gmail.com>
2024-05-05 12:22:17 +0300

    adding tests for 4th ex.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "tasks/matrix_transposition.h"
 #include "tasks/convert_float_num.h"
 #include "tasks/evaluate_expression.h"
+#include "tasks/special_word.h"
 
 
 int main() {
@@ -14,5 +15,8 @@ int main() {
     generate_expression("C:\\Users\\Vadim\\CLionProjects\\lab_work_19\\tasks_tests\\task_3_test_0.txt");
     test_evaluate();
     // 4
+    generate_string("C:\\Users\\Vadim\\CLionProjects\\lab_work_19\\tasks_tests\\task_4_test_0.txt", "abcde abcde");
+    test_filter_word();
+    // 5
 
 }
